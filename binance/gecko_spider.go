5008f7505aa1c2b672df7d299e24115b1f39200d
Peng Wu <wp.4163196@gmail.com>
2023-07-31 15:41:24 +0800

    add gecko spider

---
+++ binance/gecko_spider.go
@@ -0,0 +1,189 @@
+package main
+
+import (
+	"encoding/json"
+	"math/rand"
+	"net"
+	"net/http"
+	"net/url"
+	"os"
+	"path/filepath"
+	"strings"
+	"time"
+
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+	"gorm.io/gorm/clause"
+
+	"github.com/charmbracelet/log"
+	"github.com/philippgille/gokv"
+
+	coingecko "github.com/superoo7/go-gecko/v3"
+	coingecko_types "github.com/superoo7/go-gecko/v3/types"
+)
+
+type checkpoint struct {
+	LeftDate  time.Time
+	RightDate time.Time
+}
+
+type GeckoConfig struct {
+	DSN string `yaml:"dsn"`
+	//KVStorePath   string `yaml:"kvstore_path"`
+	DataRoot     string `yaml:"data_root"`
+	CoinListPath string `yaml:"coin_list_path"`
+}
+
+type GeckoSpider struct {
+	cgClient     *coingecko.Client
+	kvStore      gokv.Store
+	db           *gorm.DB
+	coinListPath string
+	dataPath     string
+}
+
+type GeckoMarket struct {
+	gorm.Model
+
+	GeckoId      string    `gorm:"column:gecko_id;primaryKey"`
+	Symbol       string    `gorm:"column:symbol"`
+	Name         string    `gorm:"column:name"`
+	Timestamp    int64     `gorm:"column:timestamp;primaryKey"`
+	DateTime     time.Time `gorm:"column:datetime"`
+	PriceUsd     float64   `gorm:"column:price_usd"`
+	MarketCapUsd float64   `gorm:"column:market_cap_usd"`
+	Volume24hUsd float64   `gorm:"column:volume_24h_usd"`
+}
+
+func NewGeckoSpider(configFile string) *GeckoSpider {
+	var config GeckoConfig
+	initConfig(configFile, &config)
+
+	db, err := gorm.Open(postgres.Open(config.DSN), &gorm.Config{})
+	if err != nil {
+		log.Fatal("failed to connect database", err)
+	}
+
+	proxyUrl, _ := url.Parse("http://192.168.100.1:8443")
+	cgClient := coingecko.NewClient(&http.Client{Timeout: 10 * time.Second, Transport: &http.Transport{Proxy: http.ProxyURL(proxyUrl)}})
+	kvstorePath := filepath.Join(config.DataRoot, "gokv")
+	dataPath := filepath.Join(config.DataRoot, "coin_history")
+
+	kvStore := NewKvstore(kvstorePath)
+	return &GeckoSpider{
+		db:       db,
+		cgClient: cgClient,
+		kvStore:  kvStore,
+		dataPath: dataPath,
+	}
+}
+
+func (s *GeckoSpider) ProducerCallback(jobCh chan interface{}) {
+	coins, err := s.cgClient.CoinsList()
+	if err != nil {
+		log.Fatal("failed to get coin list in producer", err)
+	}
+
+	r, _ := json.Marshal(coins)
+	if err = os.WriteFile(s.coinListPath, r, 0644); err != nil {
+		log.Error("failed to write coin list", err)
+	}
+
+	for _, coin := range *coins {
+		path := filepath.Join(s.dataPath, coin.ID)
+		if err := os.MkdirAll(path, os.ModePerm); err != nil {
+			log.Error("failed to create directory", path, err)
+		}
+
+		jobCh <- coin
+	}
+
+	log.Info("all coins sent")
+}
+
+func (s *GeckoSpider) ConsumerCallback(jobCh chan interface{}) {
+	for coin := range jobCh {
+		s.syncCoinHistory(coin.(coingecko_types.CoinsListItem), s.cgClient)
+	}
+}
+
+func (s *GeckoSpider) syncCoinHistory(coin coingecko_types.CoinsListItem, cgClient *coingecko.Client) {
+	cp := &checkpoint{}
+	found, err := s.kvStore.Get(coin.ID, cp)
+	if err != nil {
+		log.Error("failed to get checkpoint of", coin, err)
+		return
+	}
+
+	if found && !cp.LeftDate.IsZero() && !cp.RightDate.IsZero() {
+		return //already synced
+	}
+
+	retryDuration := 30 * time.Second
+
+	for {
+		coinMarkets, err := cgClient.CoinsIDMarketChart(coin.ID, "usd", "max")
+
+		if err != nil {
+			if err1, ok := err.(net.Error); ok && err1.Timeout() {
+				log.Warn("timeout ", err1)
+			} else {
+				if strings.Contains(err.Error(), "429") {
+					log.Warn("too many requests, sleep ", retryDuration, ", ", coin, ", ", err)
+				} else {
+					log.Error("failed to get coin history of ", coin, ", ", err)
+				}
+			}
+
+			time.Sleep(retryDuration + time.Duration(rand.Intn(10))*time.Second)
+
+			continue
+		}
+
+		// r, _ := json.Marshal(coinMarkets)
+		// path := filepath.Join(dataPath, coin+".json")
+
+		// err = os.WriteFile(path, r, 0644)
+		// if err != nil {
+		// 	log.Error("failed to write coin history of", coin, err)
+		// 	return
+		// }
+
+		for i := 0; i < len(*(*coinMarkets).Prices); i++ {
+			item := GeckoMarket{
+				GeckoId:      coin.ID,
+				Symbol:       coin.Symbol,
+				Name:         coin.Name,
+				Timestamp:    int64((*(*coinMarkets).Prices)[i][0]),
+				DateTime:     time.Unix(int64((*(*coinMarkets).Prices)[i][0]/1000), 0),
+				PriceUsd:     float64((*(*coinMarkets).Prices)[i][1]),
+				MarketCapUsd: float64((*(*coinMarkets).MarketCaps)[i][1]),
+				Volume24hUsd: float64((*(*coinMarkets).TotalVolumes)[i][1]),
+			}
+
+			// Upsert
+			result := s.db.Table("blockchain_overview.gecko_markets").Clauses(clause.OnConflict{
+				Columns:   []clause.Column{{Name: "gecko_id"}, {Name: "timestamp"}},                            // Use the "id" column to determine if a record exists
+				DoUpdates: clause.AssignmentColumns([]string{"price_usd", "market_cap_usd", "volume_24h_usd"}), // If a record exists, update the "name" and "age_x" fields
+			}).Create(&item)
+
+			if result.Error != nil {
+				log.Fatal("Failed to upsert ", coin, ", ", result.Error)
+			}
+		}
+
+		cp.LeftDate = time.Unix(int64((*(*coinMarkets).Prices)[0][0]/1000), 0)
+		cp.RightDate = time.Unix(int64((*(*coinMarkets).Prices)[len(*(*coinMarkets).Prices)-1][0]/1000), 0)
+
+		err = s.kvStore.Set(coin.ID, cp)
+
+		if err != nil {
+			log.Error("failed to update checkpoint of", coin, err)
+			return
+		}
+
+		log.Info("synced coin history of ", coin)
+
+		break
+	}
+}
